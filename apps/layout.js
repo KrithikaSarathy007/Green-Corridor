100ae43b6a5bbd2af6a6bf96c8edf2e27213fd84
KrithikaSarathy007 <krithikakameshwari06@gmail.com>
2026-01-06 23:17:11 +0530

    Create layout.js

---
+++ apps/layout.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { Toaster } from "sonner";
+
+export default function Layout({ children, currentPageName }) {
+  return (
+    <div className="min-h-screen">
+      <Toaster position="top-right" richColors />
+      {children}
+    </div>
+  );
+}
